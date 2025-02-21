df56f93944c608cff92c39e498135fb1d4077776
bsvalverde <bsvalverde@gmail.com>
2025-02-21 16:32:09 -0300

    feat: Adds schema and event listening

--- apps/indexer/ponder.schema.ts
+++ apps/indexer/ponder.schema.ts
@@ -1,6 +1,20 @@
-import { onchainTable } from 'ponder';
+import { onchainEnum, onchainTable } from "ponder";
+import { v4 as uuidv4 } from "uuid";
 
-export const example = onchainTable('example', (t) => ({
-  id: t.text().primaryKey(),
-  name: t.text(),
+export const bleuNFT = onchainTable("bleu_nft", (t) => ({
+  id: t.bigint().primaryKey(),
+  owner: t.text(),
+}));
+
+export const stakingType = onchainEnum("type", ["STAKED", "UNSTAKED"]);
+
+export const bleuNFTStaking = onchainTable("bleu_nft_staked", (t) => ({
+  id: t
+    .text()
+    .primaryKey()
+    .$default(() => uuidv4()),
+  user: t.text().notNull(),
+  tokenId: t.bigint().notNull(),
+  type: stakingType("type").notNull(),
+  timestamp: t.bigint().notNull(),
 }));
--- apps/indexer/src/index.ts
+++ apps/indexer/src/index.ts
@@ -1,5 +1,37 @@
-import { ponder } from 'ponder:registry';
+import { ponder } from "ponder:registry";
+import { bleuNFT, bleuNFTStaking } from "ponder:schema";
 
-// ponder.on("YourContract:YourEvent", (event) => {
-//   console.log(event);
-// });
+ponder.on("BleuNFT:Mint", async ({ event, context }) => {
+  const { client, contracts } = context;
+  const { BleuNFT } = contracts;
+
+  const owner = await client.readContract({
+    abi: BleuNFT.abi,
+    address: BleuNFT.address,
+    functionName: "ownerOf",
+    args: [event.args.tokenId],
+  });
+
+  await context.db.insert(bleuNFT).values({
+    id: event.args.tokenId,
+    owner,
+  });
+});
+
+ponder.on("BleuStakingContract:Staked", async ({ event, context }) => {
+  await context.db.insert(bleuNFTStaking).values({
+    user: event.args.user,
+    tokenId: event.args.tokenId,
+    type: "STAKED",
+    timestamp: event.args.timestamp,
+  });
+});
+
+ponder.on("BleuStakingContract:Unstaked", async ({ event, context }) => {
+  await context.db.insert(bleuNFTStaking).values({
+    user: event.args.user,
+    tokenId: event.args.tokenId,
+    type: "UNSTAKED",
+    timestamp: event.args.timestamp,
+  });
+});
